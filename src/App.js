44f599ae7a479fa48532d15f3d234b565469ac69
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-11-28 17:17:17 -0500

    added basic form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [ option, setOption ] = useState([])
+  const [ input, setInput ] = useState("")
+
   return (
     <div>
       <h1>Group Randomizer</h1>
+      <form>
+        <textarea
+          placeholder="Please input options here"
+          >
+        </textarea>
+        <br />
+        <button type="Submit">Submit</button>
+      </form>
     </div>
   )
 }

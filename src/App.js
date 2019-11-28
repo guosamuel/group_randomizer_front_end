6d89443b131aa55ae41d225a0099048a5a3b10ae
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-11-28 17:26:10 -0500

    added the submit function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ function App() {
   const [ option, setOption ] = useState([])
   const [ input, setInput ] = useState("")
 
+  const handleOptionInput = e => {
+    setInput(e.target.value)
+  }
+
+  const handleOptionSubmit = e => {
+    e.preventDefault()
+    setOption(option.push(input))
+  }
+
   return (
     <div>
       <h1>Group Randomizer</h1>
-      <form>
+      <form onSubmit={handleOptionSubmit}>
         <textarea
           placeholder="Please input options here"
+          value={input}
+          onChange={handleOptionInput}
+          rows='5'
+          cols='50'
           >
         </textarea>
         <br />

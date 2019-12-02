63f629272c0a2f183d742227943cf340fbccd4da
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-12-02 11:31:42 -0500

    Added initial dummy data and rendering options

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import Form from './components/Form'
 
 function App() {
-  const [ options, setOptions ] = useState([])
+  const [ options, setOptions ] = useState(["Sam", "Ryan", "Stephen", "David"])
 
   const handleOptionSubmit = (e, input) => {
     e.preventDefault()
     setOptions([...options, input])
   }
 
+  const listOfOptions = options.map( (option, index) => <li key={index}>{option}</li>)
+
   return (
     <div>
       <h1>Group Randomizer</h1>
       <Form handleOptionSubmit={handleOptionSubmit}/>
+      <br/>
+      <ol>{listOfOptions}</ol>
     </div>
   )
 }

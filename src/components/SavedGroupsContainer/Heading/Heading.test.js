41ac72668ec2f687c65ca134573899a99fc56532
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-10-09 19:33:16 -0400

    installed testing library react to leverage the render method

--- src/components/SavedGroupsContainer/Heading/Heading.test.js
+++ src/components/SavedGroupsContainer/Heading/Heading.test.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import Heading from './Heading'
 
-import { getQueriesForElement } from "@testing-library/dom"
+// import { getQueriesForElement } from "@testing-library/dom"
+import { render } from "@testing-library/react"
+
+// const render = (component) => {
+//   const div = document.createElement("div")
+//   ReactDOM.render(component, div)
+//   return getQueriesForElement(div)
+// }
 
 it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<Heading />, div)
+  render(<Heading />)
 })
 
 it("renders the correct content", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<Heading />, div)
-
-  const { getByText } = getQueriesForElement(div)
+  const { getByText } = render(<Heading />)
 
   getByText("Saved Groups")
 })

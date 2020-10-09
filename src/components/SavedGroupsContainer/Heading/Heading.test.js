ebc49f54f64a2facb79ea58f707dc25ce6c15abb
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-10-08 22:41:39 -0400

    added testing-library/dom and passed a test using that library

--- src/components/SavedGroupsContainer/Heading/Heading.test.js
+++ src/components/SavedGroupsContainer/Heading/Heading.test.js
@@ -2,7 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Heading from './Heading'
 
+import { getQueriesForElement } from "@testing-library/dom"
+
 it("renders without crashing", () => {
   const div = document.createElement("div")
   ReactDOM.render(<Heading />, div)
+})
+
+it("renders the correct content", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<Heading />, div)
+
+  const { getByText } = getQueriesForElement(div)
+
+  getByText("Saved Groups")
 })

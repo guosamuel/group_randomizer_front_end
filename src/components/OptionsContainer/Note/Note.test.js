8bc1189c594da36b46198584cadd86178075ca83
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-10-11 10:36:28 -0400

    added test cases for Note.js

--- src/components/OptionsContainer/Note/Note.test.js
+++ src/components/OptionsContainer/Note/Note.test.js
@@ -2,7 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Note from './Note'
 
+import { render } from '@testing-library/react'
+
 it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<Note />, div)
+  // const div = document.createElement("div")
+  // ReactDOM.render(<Note />, div)
+  render(<Note />)
+})
+
+it("renders the correct content", () => {
+  const { getByText } = render(<Note />)
+
+  getByText(`Note: You have the option of creating a group name for your current list of options. If you do not choose to do so, the default name will be "Saved Group #(Last Group Number)"`)
 })

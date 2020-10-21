215437d30657ecb1abd171ff9c31f20754f22a4c
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-10-21 19:05:50 -0400

    setting up the reducer, store, and Provider for Saved Groups Container unit test

--- src/containers/SavedGroupsContainer/SavedGroupsContainer.test.js
+++ src/containers/SavedGroupsContainer/SavedGroupsContainer.test.js
@@ -2,3 +2,35 @@ import React from 'react'
 import SavedGroupsContainer from './SavedGroupsContainer'
 import { createStore } from 'redux'
 import Provider from 'react-redux'
+
+import { render } from '@testing-library/react'
+
+const startingState = {
+  savedGroups: [
+    {
+      name: "Saved Group #1",
+      options: ["Bob", "Sam", "Gavin"]
+    },
+    {
+      name: "Testing",
+      options: ["Bleh"]
+    }
+  ]
+}
+
+function reducer(state = startingState, action) {
+  switch(action.type) {
+    case "SAVE_GROUP":
+      return { ...state, savedGroups: [ ...state.savedGroups, action.payload ] }
+    case "DELETE_SAVED_GROUP":
+      return { ...state, savedGroups: [ ...state.savedGroups.slice(0, action.payload, ...state.savedGroups.slice(action.payload+1)) ] }
+    default:
+      return state
+  }
+}
+
+function renderWithRedux(component, { initialState, store = createStore(reducer, initialState) } = {}) {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>)
+  }
+}
